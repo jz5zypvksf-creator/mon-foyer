ae9a86ae3015f77c8e5bb5a78120c8d04f55c94f
AlainWDB <jz5zypvksf@privaterelay.appleid.com>
2026-09-19 10:02:14 +0200

    fix: update Taxes savings standing order

--- src/savingsOrderRules.js
+++ src/savingsOrderRules.js
@@ -4,7 +4,7 @@ export const SAVINGS_ORDER_RULES = [
   { op: '18833987', bucket: 'vacances', label: 'Vacances / Loisirs', expectedMonthly: 100 },
   { op: '18833985', bucket: 'frais_maison', label: 'Frais divers maison / foyer', expectedMonthly: 100 },
   { op: '18838193', bucket: 'garage', label: 'Garage / Entretien véhicule', expectedMonthly: 100 },
-  { op: '19776928', bucket: 'taxes', label: 'Taxes / Impôts', expectedMonthly: 300 },
+  { op: '20401142', bucket: 'taxes', label: 'Taxes / Impôts', expectedMonthly: 300 },
   { op: '18893403', bucket: 'solde_peugeot', label: 'Épargne solde Peugeot', expectedMonthly: null },
   { op: '17178591', bucket: 'pension_alain', label: 'Épargne pension Alain', expectedMonthly: 110 },
   { op: '17178594', bucket: 'pension_esther', label: 'Épargne pension Esther', expectedMonthly: 110 },
